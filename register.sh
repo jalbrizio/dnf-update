8930e3c4636edbcbe606deb7c534df47705e08e9
Jeremi Albrizio <jalbrizio@gmail.com>
2026-01-08 06:39:12 -0700

    Update register.sh

--- register.sh
+++ register.sh
@@ -3,8 +3,8 @@
 # usage register.sh -h <hostname> -d <domain> -k -j
 
 #change these to match your system These will probably be depricated soon
-yourspacewalkserver=yourserver.com
-yourspacewalkserver2=yourserver2.com
+yourformanserver=yourserver.com
+yourformanserver2=yourserver2.com
 youroldsateliteserver=youroldserver.com
 VER=1.0
 REL=1
@@ -91,22 +91,24 @@ fi
 #cp -p /etc/sysconfig/rhn/up2date /etc/sysconfig/rhn/up2date-last$$
 
 #
-# REQUIRED: install the satellite SSL cert directly from the Satellite server...
-#rpm -ivh http://$yourspacewalkserver/pub/rhn-org-trusted-ssl-cert-$VER-$REL.noarch.rpm
+# Install the satellite SSL certs and the latest katello directly from the Satellite server...
+rpm -ivh http://$yourformanserver/pub/rhn-org-trusted-ssl-cert-$VER-$REL.noarch.rpm
+rpm -ivh http://$yourformanserver/pub/katello-ca-consumer-latest.noarch.rpm
+
 #rpm -ivh http://$yourspacewalkserver/pub/spacewalk-client-repo-2.2-1.el6.noarch.rpm
 #rpm -e rhn-org-trusted-ssl-cert
-#rpm -ivh http://$yourspacewalkserver2/pub/rhn-org-trusted-ssl-cert-$VER-$REL.noarch.rpm
-##wget http://ncc-1701-sw/pub/RPM-GPG-KEY-MariaDB -O /etc/pki/rpm-gpg/RPM-GPG-KEY-MariaDB
+#rpm -ivh http://$yourformanserver2/pub/rhn-org-trusted-ssl-cert-$VER-$REL.noarch.rpm
+##wget http://yourformanserver2/pub/RPM-GPG-KEY-MariaDB -O /etc/pki/rpm-gpg/RPM-GPG-KEY-MariaDB
 #rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-MariaDB
-#rpm --import http://$yourspacewalkserver/pub/RPM-GPG-KEY-MariaDB
-#rpm --import http://$yourspacewalkserver/pub/RPM-GPG-KEY-EPEL-6
-#rpm --import http://$yourspacewalkserver/pub/RPM-GPG-KEY-mccdrpm
-#rpm --import http://$yourspacewalkserver/pub/RPM-GPG-KEY-percona
+#rpm --import http://$yourformanserver/pub/RPM-GPG-KEY-MariaDB
+#rpm --import http://$yourformanserver/pub/RPM-GPG-KEY-EPEL-6
+#rpm --import http://$yourformanserver/pub/RPM-GPG-KEY-mccdrpm
+#rpm --import http://$yourformanserver/pub/RPM-GPG-KEY-percona
 
 # edit the Red Hat target in /etc/sysconfig/rhn/up2date to point to
 # the Satellite URL, then run  rhn_register  as usual.
 #sed -i 's/https:\/\/xmlrpc.rhn.redhat.com/https:\/\/$youroldsateliteserver/' /etc/sysconfig/rhn/up2date
-#sed -i 's/https:\/\/$youroldsateliteserver/https:\/\/$yourspacewalkserver2/' /etc/sysconfig/rhn/up2date
+#sed -i 's/https:\/\/$youroldsateliteserver/https:\/\/$yourformanserver2/' /etc/sysconfig/rhn/up2date
 #sed -i 's/RHNS-CA-CERT/RHN-ORG-TRUSTED-SSL-CERT/' /etc/sysconfig/rhn/up2date
 
 #REJOINTSAT
