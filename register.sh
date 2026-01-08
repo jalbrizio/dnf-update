50041376b8474ee672a31cd7510dbb58ce2c491b
Jeremi Albrizio <jalbrizio@gmail.com>
2026-01-07 23:22:30 -0700

    Update register.sh
    added switches to the script

--- register.sh
+++ register.sh
@@ -1,31 +1,62 @@
 #/bin/bash
 # register.sh file to register to rh satellite
+# usage register.sh -h <hostname> -d <domain> -k -j
 
+#change these to match your system These will probably be depricated soon
+yourspacewalkserver=yourserver.com
+yourspacewalkserver2=yourserver2.com
+youroldsateliteserver=youroldserver.com
+VER=1.0
+REL=1
 
-#HOSTNAME
-if [[ -z "$1" ]]; then
-read -p "Enter the hostname: " HOSTNAME1
-else 
-HOSTNAME1=$1
-echo "The hostname is $HOSTNAME1 ."
-fi
-#DOMAIN
-if [[ -z "$2" ]]; then 
-read -p "Enter the doamin: " DOMAIN1
-else 
-DOMAIN1=$2
-echo "the domain is $DOMAIN1 ."
-fi
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        -d | --domain)
+            if [ -n "$2" ] && [ "$2" != "--"* ] && [ "$2" != "-"* ]; then
+                DOMAIN1="$2"
+                DN=Y
+                shift 2 # Shift past the option and its argument
+            else
+                echo "Error: Option $1 requires an argument."
+                usage
+            fi
+            ;;
+        -h | --hostname)
+            if [ -n "$2" ] && [ "$2" != "--"* ] && [ "$2" != "-"* ]; then
+                HOSTNAME1="$2"
+                HN=Y
+                shift 2 # Shift past the option and its argument
+            else
+                echo "Error: Option $1 requires an argument."
+                usage
+            fi
+            ;;
+        -k | --sshkeys)
+            NEWSSH=Y
+            shift
+            ;;
+        -j | --joinsat)
+            NEWSAT=Y
+            shift
+            ;;
+        *)
+            echo "Error: Unknown option $1"
+            usage
+            ;;
+    esac
+done
+
+NEWHN=$(echo "$NEWHN" | tr '[:upper:]' '[:lower:]')
+NEWDN=$(echo "$NEWDN" | tr '[:upper:]' '[:lower:]')
+if [[ "$HN" == "yes" && "$DN" == "yes" ]] || [[ "$NEWHN" == "y" && "$NEWDN" == "y" ]]; then
 #HOSTNAME
 echo "Setting the hostname to $HOSTNAME1.$DOMAIN1"
 hostnamectl set-hostname $HOSTNAME1.$DOMAIN1
+else
+echo "You chose NOT to set the hostname."
+fi
 
 #SSHKEYS
-if [[ -z "$3" ]]; then 
-read -p "do you want to update the ssh keys? Y/N: " NEWSSH
-else 
-NEWSSH=$3
-fi
 NEWSSH=$(echo "$NEWSSH" | tr '[:upper:]' '[:lower:]')
 if [[ "$NEWSSH" == "yes" ]] || [[ "$NEWSSH" == "y" ]]; then
 echo "You chose to regenerate the servers ssh keys"
@@ -37,12 +68,34 @@ systemctl restart sshd
 else
 echo "You chose NOT to regenerate the servers ssh keys"
 fi
+
+#
+# register a RHEL5 or 6 client to the RHN Satellite server in the traditional manner
+#
+# this will overwrite the existing /etc/sysconfig/rhn/up2date, so...
+cp -p /etc/sysconfig/rhn/up2date /etc/sysconfig/rhn/up2date-last$$
+
+#
+# REQUIRED: install the satellite SSL cert directly from the Satellite server...
+rpm -ivh http://$yourspacewalkserver/pub/rhn-org-trusted-ssl-cert-$VER-$REL.noarch.rpm
+rpm -ivh http://$yourspacewalkserver/pub/spacewalk-client-repo-2.2-1.el6.noarch.rpm
+rpm -e rhn-org-trusted-ssl-cert
+rpm -ivh http://$yourspacewalkserver2/pub/rhn-org-trusted-ssl-cert-$VER-$REL.noarch.rpm
+#wget http://ncc-1701-sw/pub/RPM-GPG-KEY-MariaDB -O /etc/pki/rpm-gpg/RPM-GPG-KEY-MariaDB
+#rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-MariaDB
+rpm --import http://$yourspacewalkserver/pub/RPM-GPG-KEY-MariaDB
+rpm --import http://$yourspacewalkserver/pub/RPM-GPG-KEY-EPEL-6
+rpm --import http://$yourspacewalkserver/pub/RPM-GPG-KEY-mccdrpm
+rpm --import http://$yourspacewalkserver/pub/RPM-GPG-KEY-percona
+
+# edit the Red Hat target in /etc/sysconfig/rhn/up2date to point to
+# the Satellite URL, then run  rhn_register  as usual.
+sed -i 's/https:\/\/xmlrpc.rhn.redhat.com/https:\/\/$youroldsateliteserver/' /etc/sysconfig/rhn/up2date
+sed -i 's/https:\/\/$youroldsateliteserver/https:\/\/$yourspacewalkserver2/' /etc/sysconfig/rhn/up2date
+sed -i 's/RHNS-CA-CERT/RHN-ORG-TRUSTED-SSL-CERT/' /etc/sysconfig/rhn/up2date
+
 #REJOINTSAT
-if [[ -z "$4" ]]; then 
-read -p "do you want to rejoin to sattelite? Y/N: " NEWSAT
-else 
-NEWSAT=$4
-fi
+
 NEWSAT=$(echo "$NEWSAT" | tr '[:upper:]' '[:lower:]')
 if [[ "$NEWSAT" == "yes" ]] || [[ "$NEWSAT" == "y" ]]; then
 echo "You chose to rejoin to the satellite server"
@@ -63,44 +116,17 @@ subscription-manager clean
 # Enable the network 
 #
 # Register with the Satellite server
-#Use this if the key stops working. -- subscription-manager register --auto-attach 
-set -o pipefail && curl -sS  yourSATKEYURL | bash
+#Use this key if you created a key to register. -- set -o pipefail && curl -sS  YOUR-SATELLITE-KEY | bash
+subscription-manager register --auto-attach 
 else
 echo "You chose NOT to rejoin to the satellite server"
 fi
 
 
-#
-# register a RHEL5 or 6 client to the RHN Satellite server in the traditional manner
-#
-# this will overwrite the existing /etc/sysconfig/rhn/up2date, so...
-cp -p /etc/sysconfig/rhn/up2date /etc/sysconfig/rhn/up2date-last$$
-yourspacewalkserver=yourserver.com
-yourspacewalkserver2=yourserver2.com
-youroldsateliteserver=youroldserver.com
-#
-# REQUIRED: install the satellite SSL cert directly from the Satellite server...
-rpm -ivh http://$yourspacewalkserver/pub/rhn-org-trusted-ssl-cert-1.0-1.noarch.rpm
-rpm -ivh http://$yourspacewalkserver/pub/spacewalk-client-repo-2.2-1.el6.noarch.rpm
-rpm -e rhn-org-trusted-ssl-cert
-rpm -ivh http://$yourspacewalkserver2/pub/rhn-org-trusted-ssl-cert-1.0-1.noarch.rpm
-#wget http://ncc-1701-sw/pub/RPM-GPG-KEY-MariaDB -O /etc/pki/rpm-gpg/RPM-GPG-KEY-MariaDB
-#rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-MariaDB
-rpm --import http://$yourspacewalkserver/pub/RPM-GPG-KEY-MariaDB
-rpm --import http://$yourspacewalkserver/pub/RPM-GPG-KEY-EPEL-6
-rpm --import http://$yourspacewalkserver/pub/RPM-GPG-KEY-mccdrpm
-rpm --import http://$yourspacewalkserver/pub/RPM-GPG-KEY-percona
-
-# edit the Red Hat target in /etc/sysconfig/rhn/up2date to point to
-# the Satellite URL, then run  rhn_register  as usual.
-sed -i 's/https:\/\/xmlrpc.rhn.redhat.com/https:\/\/$youroldsateliteserver/' /etc/sysconfig/rhn/up2date
-sed -i 's/https:\/\/$youroldsateliteserver/https:\/\/$yourspacewalkserver2/' /etc/sysconfig/rhn/up2date
-sed -i 's/RHNS-CA-CERT/RHN-ORG-TRUSTED-SSL-CERT/' /etc/sysconfig/rhn/up2date
-
 # clean any existing yum configuration information...
-yum clean all
+dnf clean all
 
-# register the client using rhn_register and a userid and passwd...
-rhn_register
+# register the client using rhn_register and a userid and passwd... This is depticated 
+#rhn_register
 
-# all done - run  yum update  whenever ready...
+# all done - run  dnf update  whenever ready...
